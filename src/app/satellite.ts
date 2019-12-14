99250e082209509f718d470d2caf0be73f025ea8
Svagtlys <45446348+Svagtlys@users.noreply.github.com>
2019-12-14 16:13:37 -0600

    Completed part 6

--- src/app/satellite.ts
+++ src/app/satellite.ts
@@ -12,4 +12,12 @@ export class Satellite {
         this.type = type;
         this.operational = operational;
     }
+
+    shouldShowWarning(): boolean {
+        if(this.type.toLowerCase() === 'space debris'){
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
